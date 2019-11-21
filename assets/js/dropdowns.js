3f3ee384593bacb5b2e3cf2a951cf9e8d2680547
Joey Botelho <Botelho.J@Outlook.com>
2019-11-20 20:23:50 -0500

    small changes

---
+++ assets/js/dropdowns.js
@@ -0,0 +1,12 @@
+const printArea = document.querySelector("#content");
+
+const dropdown = () => {
+  const component = document.createElement("div");
+
+  const input = createInput();
+  const dropdown = showDropdown();
+
+  component.appendChild(input);
+  component.appendChild(dropdown);
+  printArea.appendChild(component);
+};
